81e058dcc992a31488f63a1893779267985fa2c6
wesley sharpe <wesmon50@yahoo.com>
2020-09-14 22:31:20 -0400

    using correct things for fibonacci

---
+++ Fibonacci.java
@@ -0,0 +1,54 @@
+/*
+ * File: BlankClass.java
+ * ---------------------
+ * This class is a blank one that you can change at will. Remember, if you change
+ * the class name, you'll need to change the filename so that it matches.
+ * Then you can extend GraphicsProgram, ConsoleProgram, or DialogProgram as you like.
+ */
+
+
+import acm.program.*;
+
+
+
+public class Fibonacci extends ConsoleProgram {
+	private static final int MaxResult = 10000;
+	public void run() {
+		/* You fill this in */
+		boolean check = false;
+		int fibPlace = 1;
+		int fibPlace2=0;
+		println("Hello please insert an integer");
+
+		do {
+			try 
+			{
+		int start = readInt();
+
+			
+			if(start > MaxResult) {
+				println("Please use a number less than "+MaxResult);
+			}
+
+			
+			else {
+				check = true;
+				while(start<MaxResult) {
+					println(start);
+					fibPlace2 =start;
+					fibPlace = fibPlace + fibPlace2;
+					start = fibPlace;
+					fibPlace = fibPlace2;
+					
+					
+				}
+			}
+		}
+		 catch(NumberFormatException e)
+		{
+			 println("Please input a number ");
+		 }
+		}while(check == false);
+	}
+}
+
---
+++ RobotHead.java
@@ -0,0 +1,6 @@
+import acm.program.*;
+
+
+public class RobotHead extends GraphicsProgram {
+
+}
