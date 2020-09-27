bbb4927be60af86806eae8229e1442ff8306909a
wesley sharpe <sharpe.wesley.ws@gmail.com>
2020-09-27 01:04:36 -0400

    cleaner

--- Fibonacci.java
+++ Fibonacci.java
@@ -6,45 +6,38 @@
  * Then you can extend GraphicsProgram, ConsoleProgram, or DialogProgram as you like.
  */
 
-
 import acm.program.*;
-
-
 
 public class Fibonacci extends ConsoleProgram {
 	private static final int MaxResult = 10000;
+
 	public void run() {
 		/* You fill this in */
 		boolean check = false;
 		int fibPlace = 1;
-		int fibPlace2=0;
+		int fibPlace2 = 0;
 		println("Hello please insert an integer");
 
 		do {
-		int start = readInt();
+			int start = readInt();
 
-			
-			if(start > MaxResult) {
-				println("Please use a number less than "+MaxResult);
+			if (start > MaxResult) {
+				println("Please use a number less than " + MaxResult);
 			}
 
-			
 			else {
 				check = true;
-				while(start<MaxResult) {
+				while (start < MaxResult) {
 					println(start);
-					fibPlace2 =start;
+					fibPlace2 = start;
 					fibPlace = fibPlace + fibPlace2;
 					start = fibPlace;
 					fibPlace = fibPlace2;
-					
-					
+
 				}
 			}
 		}
 
-
-		while(check == false);
+		while (check == false);
 	}
 }
-
--- RobotHead.java
+++ RobotHead.java
@@ -1,38 +1,49 @@
 import java.awt.Color;
 
-
 import acm.graphics.*;
 import acm.program.*;
 
-
 public class RobotHead extends GraphicsProgram {
 
-	public static final int HEAD_WIDTH= 250;
-	public static final int HEAD_HEIGHT=400;
-	public static final double EYE_RADIUS =25;
-	public static final int MOUTH_WIDTH =175;
-	public static final int MOUTH_HEIGHT =50;
+	public static final int HEAD_WIDTH = 250;
+	public static final int HEAD_HEIGHT = 400;
+	public static final double EYE_RADIUS = 25;
+	public static final int MOUTH_WIDTH = 175;
+	public static final int MOUTH_HEIGHT = 50;
 
-	
-	
 	public void run() {
 		double ELength = EYE_RADIUS * 2;
-		GRect face = new GRect(getWidth()/2, getHeight()/2,HEAD_WIDTH, HEAD_HEIGHT);
-		GOval eye1 = new GOval(150, 200,ELength, ELength);
-		GRect mouth = new GRect(135,400,MOUTH_WIDTH,MOUTH_HEIGHT);
-		GOval eye2 = new GOval(250, 200,ELength, ELength);
+		GRect face = new GRect(getWidth() / 2, getHeight() / 2, HEAD_WIDTH, HEAD_HEIGHT);
+		GOval eyes = new GOval(ELength, ELength);
+		GRect mouth = new GRect(135, 400, MOUTH_WIDTH, MOUTH_HEIGHT);
+
 		face.setFilled(true);
 		face.setColor(Color.black);
 		face.setFillColor(Color.gray);
-		eye1.setFillColor(Color.yellow);
-		eye1.setFilled(true);
-		eye2.setFillColor(Color.yellow);
-		eye2.setFilled(true);
 		mouth.setFillColor(Color.white);
 		mouth.setFilled(true);
 		add(face);
-		add(eye1);
-		add(eye2);
 		add(mouth);
+		makeEyes(ELength);
+	}
+
+	private void makeEyes(double eLength) {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < 2; i++) {
+			if (i == 0) {
+				GOval eyes = new GOval(150, 200, eLength, eLength);
+				eyes.setFilled(true);
+				eyes.setColor(Color.yellow);
+				add(eyes);
+			} else {
+				GOval eyes = new GOval(250, 200, eLength, eLength);
+				eyes.setFilled(true);
+				eyes.setColor(Color.yellow);
+				add(eyes);
+
+			}
+
+		}
+
 	}
 }
